cc5e13429076cd3b301ae45bd72b2bf609c8a1f7
yashmehta001 <yashmehta001@yahoo.in>
2022-09-21 02:56:30 +0530

    removed and changed api key

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -1,8 +1,7 @@
 const sgMail = require("@sendgrid/mail");
 
 const sendgridAPIKey = process.env.SENDGRID_API_KEY;
-//   "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";
-//
+
 sgMail.setApiKey(sendgridAPIKey);
 
 const sendWelcomeEmail = (email, name) => {
